0cc66dc42112c9e293b5601c73e99ba58dea8156
Finn <112250336+finn200102@users.noreply.github.com>
2024-12-16 18:04:08 +0100

    add: add_item

--- src/order.py
+++ src/order.py
@@ -1,1 +1,8 @@
-"""this is my order"""
+"""Order module"""
+class Order:
+    def __init__(self):
+        self.items = []
+    
+    def add_item(self, order_item):
+        """Adds an order to the items list"""
+        self.items.append(order_item)
