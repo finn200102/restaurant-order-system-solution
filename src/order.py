d6c1e6d80e664bd73e54a4e9a5d90edcd8b6e0c5
Finn <112250336+finn200102@users.noreply.github.com>
2024-12-18 23:21:09 +0100

    feat: Implement add_special_request_to_item method

--- src/order.py
+++ src/order.py
@@ -7,13 +7,18 @@ class Order:
         """Adds an order to the items list"""
         self.items.append(order_item)
 
-    def remove_item(self, idx):
+    def remove_item(self, idx: int):
         """Removes an item of the order"""
         if idx >= 0 and idx < len(self.items):
             self.items.pop(idx)
             return True
         else:
             return False
+        
+    def add_special_requests_to_item(self, idx: int, special_requests):
+        """Adds list of special requests to a specific orderitem of the order"""
+        self.items[idx].special_request += special_requests
+
         
     def get_total_price(self):
         """Calculates and returns the total price of the order"""
