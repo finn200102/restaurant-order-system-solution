a9ec0afb59c44a17792541422bbf9d070afcd093
Finn <112250336+finn200102@users.noreply.github.com>
2024-12-18 23:20:43 +0100

    test: Add unit test for Order.add_special_request_to_item

--- tests/test_order.py
+++ tests/test_order.py
@@ -17,17 +17,27 @@ def test_remove_item(order):
     print("test_remove_item passed")
 
 
-def get_total_price(order):
+def test_get_total_price(order):
     """Test get total price of all order items"""
     total_price = order.get_total_price()
     assert total_price == 5.6 + 1.0
-    print("get_total_price passed")
+    print("test_get_total_price passed")
+
+
+def test_add_special_requests_to_item(order):
+    """Test add special requests to specific item of an order"""
+    order_item = OrderItem("pommes", ["marmelade"], 5.6)
+    order.add_item(order_item)
+    order.add_special_requests_to_item(0, ["ketchup", "wine"])
+    assert len(order.items[0].special_request) == 3
+    print("test_add_special_request_to_item passed")
 
 
 
 if __name__ == "__main__":
     order = Order()
     test_add_item(order)
-    get_total_price(order)
+    test_get_total_price(order)
     test_remove_item(order)
+    test_add_special_requests_to_item(order)
     
