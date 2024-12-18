15967c4f558c65507c006a2a6f7e3145fcb933d6
Deine Mutter <deine.Mutter@gmail.com>
2024-12-18 20:07:12 +0100

    add menue class and menue tests

--- src/menue.py
+++ src/menue.py
@@ -1,3 +1,39 @@
-class Menue:
-    def __init__(self):
-        print("hi")
+"""Menue Module"""
+
+import csv
+
+class Menu:
+    def __init__(self, filename: str):
+        self.items: dict[str, float] = {}
+        self.menu(filename)
+
+    def menu(self, filename: str):
+        """creates the menu"""
+        try:
+            with open(filename, mode='r', encoding='utf-8') as file:
+                reader = csv.DictReader(file, delimiter=';')
+                for row in reader:
+                    name = row['name']
+                    try:
+                        price = float(row['price'].replace(',', '.'))
+                        self.items[name] = price
+                    except ValueError:
+                        print(f"Fehler beim Konvertieren des Preises für '{name}'.")
+        except FileNotFoundError:
+            print(f"Fehler: Die Datei '{filename}' wurde nicht gefunden.")
+        except KeyError:
+            print("Fehler: Die Datei hat nicht die erwarteten Spalten 'name' und 'price'.")
+
+    def addItem(self, name: str, price: float) -> None:
+        """Adds an item to the menu"""
+        self.items[name] = price
+
+    def getItemPrice(self, name: str):
+        """gets the price of an item"""
+        return self.items.get(name)
+
+    def displayMenu(self):
+        """outputs the menu"""
+        for name, price in self.items.items():
+            print(f"{name}: {price:.2f} €")
+
--- tests/test_menue.py
+++ tests/test_menue.py
@@ -1,3 +1,14 @@
-from src.menue import Menue
+"""Tests for the Menue Class"""
+from src.menue import Menu
 
-m = Menue()
+
+m = Menu.menu("food.csv")
+"""tests the output from the menu"""
+m.displayMenu()
+
+"""Tests the addition of items"""
+m.addItem(str.upper("Vodka",15.0))
+m.displayMenu()
+
+"""tests the award of a prize"""
+print(m.etItemPrice(str.upper("fanta (0.4)")))
