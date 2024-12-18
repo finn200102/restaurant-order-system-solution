b3639bc40bc2384f92e6e6a65c66d9f719212f2d
Finn <112250336+finn200102@users.noreply.github.com>
2024-12-18 16:49:34 +0100

    feat: Implement remove_item method for Order class

--- src/order.py
+++ src/order.py
@@ -6,3 +6,11 @@ class Order:
     def add_item(self, order_item):
         """Adds an order to the items list"""
         self.items.append(order_item)
+
+    def remove_item(self, idx):
+        """Removes an item of the order"""
+        if idx >= 0 and idx < len(self.items):
+            self.items.pop(idx)
+            return True
+        else:
+            return False
