cb148aa5c5a6f356643c37724592eb4fe8093a48
Finn <112250336+finn200102@users.noreply.github.com>
2024-12-18 22:35:12 +0100

    test: Add unit test for Order.get_total_price

--- tests/test_order.py
+++ tests/test_order.py
@@ -5,7 +5,7 @@ from src.order_item import OrderItem
 
 def test_add_item(order):
     """Test adding one item to an Order"""
-    order_item = OrderItem("pommes", "marmelade", 5.6)
+    order_item = OrderItem("pommes", ["marmelade"], 5.6)
     order.add_item(order_item)
     assert order.items[0].name == "pommes"
     print("test_add_items passed")
@@ -17,9 +17,17 @@ def test_remove_item(order):
     print("test_remove_item passed")
 
 
+def get_total_price(order):
+    """Test get total price of all order items"""
+    total_price = order.get_total_price()
+    assert total_price == 5.6 + 1.0
+    print("get_total_price passed")
+
 
 
 if __name__ == "__main__":
     order = Order()
     test_add_item(order)
+    get_total_price(order)
     test_remove_item(order)
+    
