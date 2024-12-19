2fb2bbd4b239a8a3d18268a022ae6cd19921ad23
Finn <112250336+finn200102@users.noreply.github.com>
2024-12-19 14:44:10 +0100

    test: Add unit test for Restaurant.get_table

--- tests/test_restaurant.py
+++ tests/test_restaurant.py
@@ -12,12 +12,26 @@ def test_add_table():
     order.add_item(order_item)
     table = Table(1)
     table.add_order(order)
-    menue = Menu("food.csv")
+    menue = Menu("src/food.csv")
     restaurant = Restaurant(menue=menue)
     restaurant.add_table(table)
     assert restaurant.tables[0] == table
     print("test_add_table passed")
 
+def test_get_table():
+    """Test the get_table function"""
+    order = Order()
+    order_item = OrderItem("pommes", ["marmelade"], 5.6)
+    order.add_item(order_item)
+    table = Table(1)
+    table.add_order(order)
+    menue = Menu("src/food.csv")
+    restaurant = Restaurant(menue=menue)
+    restaurant.add_table(table)
+    assert restaurant.get_table(1) == table
+    print("test_get_table passed")
+
 
 if __name__ == "__main__":
     test_add_table()
+    test_get_table()
