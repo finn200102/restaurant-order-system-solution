dee098211a2810831bf90173e630ae955d5b4c29
Finn <112250336+finn200102@users.noreply.github.com>
2024-12-19 15:05:56 +0100

    feat: Implement save_bill method

--- src/restaurant.py
+++ src/restaurant.py
@@ -12,3 +12,9 @@ class Restaurant:
         """Returns the table with the idx"""
         matches =  [t for t in self.tables if t.table_number == idx]
         return matches[0] if matches else None
+    
+    def save_bill(self, idx, filepath):
+        """Saves the bill to a txt"""
+        bill = self.get_table(idx).get_bill()
+        with open(filepath, "a", encoding="utf-8") as f:
+            f.write(f"table number: {idx}, price: {bill}\n")
