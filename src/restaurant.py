196d24109a9ff90fa170483ef3e7f5b35444bab8
Finn <112250336+finn200102@users.noreply.github.com>
2024-12-19 14:44:37 +0100

    feat: Implement get_table method

--- src/restaurant.py
+++ src/restaurant.py
@@ -7,3 +7,8 @@ class Restaurant:
     def add_table(self, table):
         """Add a table to the restaurant"""
         self.tables.append(table)
+
+    def get_table(self, idx):
+        """Returns the table with the idx"""
+        matches =  [t for t in self.tables if t.table_number == idx]
+        return matches[0] if matches else None
