1eeed2affb1e6ee555c5dfffb97516a46bf2cf0b
Finn <112250336+finn200102@users.noreply.github.com>
2024-12-20 16:37:13 +0100

    feat: Implement add_order_to_table, remove_order_from_table

--- src/restaurant.py
+++ src/restaurant.py
@@ -1,3 +1,5 @@
+from src.order_item import OrderItem
+from src.order import Order
 class Restaurant:
     def __init__(self, menue):
         """Initializes the Restaurant class"""
@@ -12,6 +14,16 @@ class Restaurant:
         """Returns the table with the idx"""
         matches =  [t for t in self.tables if t.table_number == idx]
         return matches[0] if matches else None
+    
+    def add_order_to_table(self, idx, name, special_request, base_price):
+        """Adds an order to a table"""
+        order = Order()
+        order.add_item(OrderItem(name, special_request, base_price))
+        self.get_table(idx).add_order(order)
+
+    def remove_order_from_table(self, idx, order_idx):
+        """Remove the order_idx order from table idx"""
+        self.get_table(idx).remove_order(order_idx)
     
     def save_bill(self, idx, filepath):
         """Saves the bill to a txt"""
