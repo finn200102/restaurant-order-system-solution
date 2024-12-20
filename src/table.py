ecfd19e9e968d566d1512a7238869f16f5d9ed48
Finn <112250336+finn200102@users.noreply.github.com>
2024-12-20 21:52:19 +0100

    make pylint happy

--- src/table.py
+++ src/table.py
@@ -1,24 +1,29 @@
-"""This is the table module for the Table class"""
+"""This is the table module for the Table class."""
 __author__ = "7157747, Gellien, 8425470, Heidusch"
-from src.order import Order
+
+
 class Table:
+    """This is the Table class."""
+
     def __init__(self, table_number: int):
-        """Init the table class"""
+        """Init the table class."""
         self.table_number = table_number
         self.orders = []
-        
+
     def add_order(self, order):
-        """Add order to orders"""
+        """Add order to orders."""
         self.orders.append(order)
 
     def get_bill(self):
-        """Returns the total_price of all orders on the table"""
-        return sum([order.get_total_price() for order in self.orders])
-    
+        """Return the total_price of all orders on the table."""
+        return sum(
+            order.get_total_price()
+            for order in self.orders
+        )
+
     def remove_order(self, idx):
-        """Remove an order at the index idx"""
-        if idx >= 0 and idx < len(self.orders):
+        """Remove an order at the index idx."""
+        if 0 <= idx < len(self.orders):
             self.orders.pop(idx)
             return True
-        else:
-            return False
+        return False
