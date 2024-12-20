c4e7ceb880afca34b2958d1903ce35c4608fb0e1
Finn <112250336+finn200102@users.noreply.github.com>
2024-12-20 15:40:19 +0100

    feat: add boilerplate for the main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+"""This it the main loop module for the restaurant managment system"""
+
+
+def main():
+    """The main loop of the management sytem"""
+    
+
+
+if __name__ == "__main__":
+    main()
